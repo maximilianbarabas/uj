93a02e31d8abaa14950239128769059b1cc1b2dd
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-03-31 23:54:42 +0200

    created leftsidebar_login_registration_rightsidebar_athomepage

--- angularproject/src/app/default/default.module.ts
+++ angularproject/src/app/default/default.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { DefaultRoutingModule } from './default-routing.module';
 import { DefaultComponent } from './default.component';
 import { DproductsComponent } from './dproducts/dproducts.component';
+import { DleftsidebarComponent } from './dleftsidebar/dleftsidebar.component';
+import { DrightsidebarComponent } from './drightsidebar/drightsidebar.component';
+import { DregistrationComponent } from './dregistration/dregistration.component';
+import { DloginComponent } from './dlogin/dlogin.component';
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DproductsComponent
+    DproductsComponent,
+    DleftsidebarComponent,
+    DrightsidebarComponent,
+    DregistrationComponent,
+    DloginComponent
   ],
   imports: [
     CommonModule,
