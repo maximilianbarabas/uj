8dc7465f9400a97c725a1fc72410d4820d1eedb5
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-03-31 10:35:09 +0200

    product_update_done

--- angularproject/src/app/admin/products/products.component.ts
+++ angularproject/src/app/admin/products/products.component.ts
@@ -15,6 +15,8 @@ export class ProductsComponent implements OnInit {
 products!: productModel[]; 
 // For Image 
 SelectedImage:File=null as any;
+tempID="";
+
 
   constructor(private proser:ProductService) {
 
@@ -61,6 +63,46 @@ fd.append('description',$("#description").val());
         },
     error =>{error.error.error.forEach((element: any)=>{toastr.error("Error", element); });
     });
-}
-}
 
+
+  }
+  selectForUpdate(id: any)
+  {
+    this.tempID=(id)
+    this.products.forEach(el=> {
+      if(id==el.id)
+      {
+        $("#uname").prop("value",el.name)
+        $("#oldcategory").prop("value",el.category)
+        $("#oldcategory").html(el.category)
+        $("#oldbrand").prop("value",el.brand)
+        $("#oldbrand").html("value",el.brand)
+        $("#price").prop("value",el.price)
+        $("#description").prop("value",el.description)
+      }
+    })
+  }
+  update()
+
+  {
+    var message;
+    const fd=new FormData();
+    fd.append('id',this.tempID);
+    fd.append('name',$("#uname").vall());
+    fd.append('category',$("#ucategory").val());
+    fd.append('brand',$("#ubrand").val());
+    fd.append('price',$("#uprice").val());
+    fd.append('description',$("#udescription").val());
+        this.proser.update(fd).subscribe (
+          res=>{
+            message=res;
+            Toast.fire({
+              type:"success",
+              title:message});
+              this.proser.getFromDb("");
+            },
+        error =>{error.error.error.forEach((element: any)=>{toastr.error("Error", element); });
+        });
+
+}
+}
