9fd38e02c06029ab34cbb71d9ebeaa076dec5c03
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-02 23:55:30 +0200

    homepage preapered

--- angularproject/src/app/default/default-routing.module.ts
+++ angularproject/src/app/default/default-routing.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'',component:DefaultComponent,
   children:[
+    {path:'',component:DproductsComponent},
     {path:'products',component:DproductsComponent}
 
   ]}
