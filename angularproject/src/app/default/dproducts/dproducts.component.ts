003cf4945dc5411e7fcc3096aea1330c48f8eb4b
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-03 00:22:43 +0200

    homepage main D product debug at ts

--- angularproject/src/app/default/dproducts/dproducts.component.ts
+++ angularproject/src/app/default/dproducts/dproducts.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dproducts',
   templateUrl: './dproducts.component.html',
   styleUrls: ['./dproducts.component.css']
 })
-export class DproductsComponent {
+export class DproductsComponent implements OnInit {
+
+  constructor(private proSer:ProductService) { }
+
+  
+  ngOnInit(): void {
+
+    }
 
 }
