e935466aca78a7956a5ed410043a08f2287d1758
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-03 00:53:22 +0200

    homepage product view almost done

--- angularproject/src/app/default/dproducts/dproducts.component.ts
+++ angularproject/src/app/default/dproducts/dproducts.component.ts
@@ -1,5 +1,6 @@
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { productModel } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-dproducts',
@@ -7,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dproducts.component.css']
 })
 export class DproductsComponent implements OnInit {
+  products!: productModel[]; 
 
   constructor(private proSer:ProductService) { }
 
   
   ngOnInit(): void {
+    this.proSer.AllProducts.subscribe(res=>{
+  
+      this.products=res
+    console.log(this.products);
+    
+    });
 
     }
 
