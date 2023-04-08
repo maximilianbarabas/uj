3c3282cae065f09178e3ab2fe37f4a43f373f224
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-08 21:28:32 +0200

    registration systeam work done

--- angularproject/src/app/default/dregistration/dregistration.component.ts
+++ angularproject/src/app/default/dregistration/dregistration.component.ts
@@ -1,4 +1,9 @@
+import { UserService } from './../../services/user.service';
 import { Component } from '@angular/core';
+
+declare var $: any
+declare var toastr: any;
+declare var Toast: any;
 
 @Component({
   selector: 'app-dregistration',
@@ -6,10 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./dregistration.component.css']
 })
 export class DregistrationComponent {
-  constructor() {}
+  constructor(private userSer:UserService) {}
 
   ngOnInit(): void {
     
   }
+  add() {
 
+    if($("#pass").val()!=$("#cpass").val())
+    {
+      toastr.error("Error","Both Password must be Same");
+    }
+    else 
+    {
+      var message;
+      const fd=new FormData();
+      fd.append('name',$("#name").vall());
+      fd.append('email',$("#email").val());
+      fd.append('password',$("#pass").val());
+          this.userSer.register("fd").subscribe (
+            res=>{
+              message=res;
+              Toast.fire({
+                type:"success",
+                title:"Successfully registered"
+                  });
+              },
+          error =>{error.error.error.forEach((element: any)=>{toastr.error("Error", element); });
+          });
+    }
+
+  }
 }
