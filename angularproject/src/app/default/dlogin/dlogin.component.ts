83e32833f95fc9d940f3027f5b4fbd961a38e25d
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-04-08 21:47:59 +0200

    login window & systeam done

--- angularproject/src/app/default/dlogin/dlogin.component.ts
+++ angularproject/src/app/default/dlogin/dlogin.component.ts
@@ -1,4 +1,9 @@
+import { UserService } from './../../services/user.service';
 import { Component } from '@angular/core';
+
+declare var $: any
+declare var toastr: any;
+declare var Toast: any;
 
 @Component({
   selector: 'app-dlogin',
@@ -7,10 +12,28 @@ import { Component } from '@angular/core';
 })
 export class DloginComponent {
 
-  constructor() {}
+  constructor(private userSer:UserService) {}
 
   ngOnInit(): void{
     
+  }
+  login() {
+
+      var message;
+      const fd=new FormData();
+      fd.append('email',$("#email").val());
+      fd.append('password',$("#pass").val());
+          this.userSer.register("fd").subscribe (
+            res=>{
+              message=res;
+              Toast.fire({
+                type:"success",
+                title:"Successfully Logged"
+                  });
+              },
+          error =>{error.error.error.forEach((element: any)=>{toastr.error("Error", element); });
+          });
+
   }
 
 }
